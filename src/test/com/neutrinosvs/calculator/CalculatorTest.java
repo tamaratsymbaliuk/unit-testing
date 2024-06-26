74882ea01b84c312d203101aad68ff4dc67fd851
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-06-25 22:15:01 -0500

    Unit testing - CalculatorTest class

--- src/test/com/neutrinosvs/calculator/CalculatorTest.java
+++ src/test/com/neutrinosvs/calculator/CalculatorTest.java
@@ -1,8 +1,13 @@
 package com.neutrinosvs.calculator;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
 
     public void canAddZeroPlusZero() {
+        Calculator calc = new Calculator();
+        int sum = calc.add(0, 0);
+        assertEquals(0, sum);
 
     }
 
