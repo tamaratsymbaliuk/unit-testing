f1db72da151553ee863dae48ec15a86e6d8f7f27
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-07-06 14:53:07 -0500

    Unit testing - CalculatorTest class

--- src/test/com/neutrinosvs/calculator/CalculatorTest.java
+++ src/test/com/neutrinosvs/calculator/CalculatorTest.java
@@ -1,6 +1,7 @@
 package com.neutrinosvs.calculator;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -27,12 +28,23 @@ public class CalculatorTest {
         assertEquals(2, sum, "Here could be a message");
 
     }
-    @Test
+    @Test()
+    @Disabled
     public void canAddMaxInputPlusOne() {
         int sum = calc.add(Integer.MAX_VALUE, 1); // 2147483647
         System.out.println(sum);
         assertEquals(Integer.MAX_VALUE + 1L, sum, "INT can only hold 32 bits");
 
+    }
+    @Test
+    public void annuityExample() {
+        String answer = calc.calcAnnuity("22000", 7, ".06", 1);
+        assertEquals("$184,664.43",answer);
+    }
+    @Test
+    public void annuityPractice2() {
+        String answer = calc.calcAnnuity("1200", 10, ".08", 4);
+        assertEquals("$72,482.38",answer);
     }
     
 
