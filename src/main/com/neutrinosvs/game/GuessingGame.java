1c3cb5cfa0134cc07245b85a3000613e0cf64504
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-07-06 17:33:19 -0500

    Unit testing - GuessingGame class

--- src/main/com/neutrinosvs/game/GuessingGame.java
+++ src/main/com/neutrinosvs/game/GuessingGame.java
@@ -1,11 +1,14 @@
 package com.neutrinosvs.game;
 
+import java.util.Random;
+
 public class GuessingGame {
+    private final int randomNumber = new Random().nextInt(10) + 1;
     public String guess(int guessedNumber) {
         return guessedNumber == getRandomNumber() ? "You got it": "You didn't get it";
     }
 
     public int getRandomNumber() {
-        return 0;
+        return randomNumber;
     }
 }
