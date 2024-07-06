d88de5f3ede6d783200a76cb2f53231c9b445f16
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-07-06 17:33:46 -0500

    GuessingGameTest

--- src/test/com/neutrinosvs/game/GuessingGameTest.java
+++ src/test/com/neutrinosvs/game/GuessingGameTest.java
@@ -1,14 +1,22 @@
 package com.neutrinosvs.game;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GuessingGameTest {
 
+    private GuessingGame game;
+
+    @BeforeEach
+    void setUp() {
+        // before each test runs our Junit calls this method so we don't do GuessingGame game = new  GuessingGame(); in each class
+        game = new  GuessingGame();
+    }
+
     @Test
     public void testSimpleWinSituation() {
-        GuessingGame game = new  GuessingGame();
         int randomNum = game.getRandomNumber();
         String message = game.guess(randomNum);
         assertEquals("You got it", message);
@@ -16,17 +24,33 @@ public class GuessingGameTest {
     }
     @Test
     public void testOneNegWrongGuessSituation() {
-        GuessingGame game = new  GuessingGame();
         String message = game.guess(-5);
         assertEquals("You didn't get it", message);
 
     }
     @Test
     public void testOneWrongPosGuessSituation() {
-        GuessingGame game = new  GuessingGame();
         int randomNum = game.getRandomNumber();
         String message = game.guess(randomNum + 1);
         assertEquals("You didn't get it", message);
+
+    }
+    @Test
+    public void testRandomNumberGeneration() {
+        // 1 2 3 4 5 6 7 8 9 10
+        // 1 1 0 1 0 1 1 1 0 1  1 = 10
+        int[] rndNumCount = new int[11];
+        for (int i = 0; i < 100; i++) {
+            GuessingGame game = new GuessingGame();
+            int randomNum = game.getRandomNumber();
+            rndNumCount[randomNum] = 1;
+        }
+        int sum = 0;
+        for (int i = 0; i < 11; i++) {
+            sum += rndNumCount[i];
+        }
+
+        assertEquals(10, sum);
 
     }
 }
