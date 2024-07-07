20e7ebbbb5ddf8f1e5ff22e18c1b69cc6645121b
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-07-06 19:47:07 -0500

    Unit testing - GuessingGame class

--- src/main/com/neutrinosvs/game/GuessingGame.java
+++ src/main/com/neutrinosvs/game/GuessingGame.java
@@ -7,12 +7,18 @@ public class GuessingGame {
     private int counter = 0;
     public String guess(int guessedNumber) {
         counter++;
-        if (counter == 4 && guessedNumber != getRandomNumber()) {
-            return "You didn't get it and you've had four tries. Game over.";
-        }
         String tryText = counter == 1 ? "try" : "tries";
         String winningMsg = String.format("You got it in %d %s", counter, tryText);
-        return guessedNumber == getRandomNumber() ? winningMsg : "You didn't get it";
+        String response = null;
+
+        if (counter == 4 && guessedNumber != getRandomNumber()) {
+            response = String.format("You didn't get it and you've had %d %s. Game over.", counter, tryText);
+        } else if (counter > 4) {
+            response = "Sorry, you are limited to only 4 tries. Game over.";
+        } else {
+            response = guessedNumber == getRandomNumber() ? winningMsg : "You didn't get it";
+        }
+        return response;
     }
 
     public int getRandomNumber() {
