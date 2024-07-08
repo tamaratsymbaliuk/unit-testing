1745673d94c7a2ad5900340f6f3a0cd29fb2c01c
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-07-08 17:57:34 -0500

    Unit testing - GuessingGame class

--- src/main/com/neutrinosvs/game/GuessingGame.java
+++ src/main/com/neutrinosvs/game/GuessingGame.java
@@ -33,4 +33,21 @@ public class GuessingGame {
     public int getRandomNumber() {
         return randomNumber;
     }
+
+    public static void main(String[] args) {
+        GuessingGame game = new GuessingGame();
+        boolean loopShouldContinue = true;
+        do {
+            String input = System.console().readLine("Enter a number: ");
+            if ("q".equals(input)) {
+                return;
+            }
+            String output = game.guess(Integer.parseInt(input));
+            System.out.println(output);
+            if (output.contains("You got it") || output.contains("over")) {
+                loopShouldContinue = false;
+            }
+        } while (loopShouldContinue);
+
+    }
 }
