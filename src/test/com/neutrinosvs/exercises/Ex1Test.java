2a40eb680277a57bae94d01db73ce3e3570edaf2
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-07-09 18:42:58 -0500

    Unit testing - Ex1Test class

--- src/test/com/neutrinosvs/exercises/Ex1Test.java
+++ src/test/com/neutrinosvs/exercises/Ex1Test.java
@@ -1,11 +1,22 @@
 package com.neutrinosvs.exercises;
 
+import com.neutrinosvs.exercieses.Ex1;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Ex1Test {
     @Test
     public void testToUpperCaseLetter() {
-       everyOtherUpperCase
+       String result = Ex1.everyOtherUpperCase("cat");
+       assertEquals("cAt", result,"Should return every other upper case");
+    }
+
+    @Test
+    public void testEmptyString() {
+        String result = Ex1.everyOtherUpperCase("");
+        assertEquals("", result,"Empty string should return empty string ");
     }
 
 }
+
