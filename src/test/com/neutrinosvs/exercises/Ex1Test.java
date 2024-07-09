b21ae2c130e097d91b0895c094f5e117d62de80a
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-07-09 12:44:49 -0500

    Unit testing - Ex1Test class

---
+++ src/test/com/neutrinosvs/exercises/Ex1Test.java
@@ -0,0 +1,11 @@
+package com.neutrinosvs.exercises;
+
+import org.junit.jupiter.api.Test;
+
+public class Ex1Test {
+    @Test
+    public void testToUpperCaseLetter() {
+       everyOtherUpperCase
+    }
+
+}
