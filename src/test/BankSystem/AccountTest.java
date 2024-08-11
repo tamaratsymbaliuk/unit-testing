07b63d99948a50d405d5d2628d061461e0382a9b
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-08-11 14:05:53 -0500

    Unit testing - Bank System

--- src/test/BankSystem/AccountTest.java
+++ src/test/BankSystem/AccountTest.java
@@ -45,16 +45,36 @@ class AccountTest {
 
     @Test
     void changePin() {
+        assertTrue(account.verifyPin("0000"));
+        account.changePin("0000","1111");
+        assertTrue(account.verifyPin("1111"));
+        // Test changing PIN with incorrect old PIN
+        account.changePin("0000", "2222");
+        assertFalse(account.verifyPin("2222"));
     }
 
     @Test
     void deposit() {
-        account.deposit(200.00);
-        assertEquals(300.00, account.getBalance());
+        account.deposit(200.0);
+        assertEquals(300.0, account.getBalance());
+
+        // Test depositing a negative amount
+        account.deposit(-300.0);
+        assertEquals(300.0, account.getBalance());
     }
 
     @Test
     void withdraw() {
+        account.withdraw(20.0);
+        assertEquals(80.0, account.getBalance());
+        // Test withdrawing an amount greater than the balance
+        account.withdraw(100.0);
+        assertEquals(80.0, account.getBalance());
+
+        // Test withdrawing a negative amount
+        account.withdraw(-10.0);
+        assertEquals(80.0, account.getBalance());
+
     }
 
     @Test
