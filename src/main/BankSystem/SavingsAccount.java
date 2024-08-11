0f26bfb0324066ccfa7d10e182b3c26fab280ff1
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-08-11 13:32:41 -0500

    Unit testing - Bank System

---
+++ src/main/BankSystem/SavingsAccount.java
@@ -0,0 +1,18 @@
+package BankSystem;
+
+public class SavingsAccount extends Account {
+    private double interestRate;
+    public SavingsAccount(String accountNumber, double balance, double interestRate) {
+        super(accountNumber, balance);
+        this.interestRate = interestRate;
+    }
+    public void applyInterest() {
+        double interest = getBalance() * interestRate;
+        deposit(interest);
+        System.out.println("Interest applied: " + interest + ". New balance: " + getBalance());
+    }
+    @Override
+    public String toString() {
+        return "SavingsAccount(" + getAccountNumber() + ", Balance: " + getBalance() + ", InterestRate: " + interestRate + "%)";
+    }
+}
