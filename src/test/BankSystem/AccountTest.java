e6426e8ee6d7fca71852b813d56d4adf5d5ed5a5
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-08-11 13:31:09 -0500

    Unit testing - Bank System

---
+++ src/test/BankSystem/AccountTest.java
@@ -0,0 +1,44 @@
+package BankSystem;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountTest {
+
+    @Test
+    void getBalance() {
+    }
+
+    @Test
+    void getAccountNumber() {
+    }
+
+    @Test
+    void getTransactionHistory() {
+    }
+
+    @Test
+    void addTransaction() {
+    }
+
+    @Test
+    void verifyPin() {
+    }
+
+    @Test
+    void changePin() {
+    }
+
+    @Test
+    void deposit() {
+    }
+
+    @Test
+    void withdraw() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
