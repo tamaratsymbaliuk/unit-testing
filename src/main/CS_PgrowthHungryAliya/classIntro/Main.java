eed7dae7cb0cd09410d2022d9b4ecdf85dd9a10b
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-08-31 18:12:19 -0500

    Your commit message here

--- src/main/CS_PgrowthHungryAliya/classIntro/Main.java
+++ src/main/CS_PgrowthHungryAliya/classIntro/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Person aliya = new Person("Aliya", 28);
         Person copy = new Person(aliya);
+        copy.setName("Tom");
 
         System.out.println("My copy object name is " + copy.getName());
         System.out.println("My copy object age is "+ copy.getAge());
