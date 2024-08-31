a4c9d584d9fd68a1737a2032a2965c882680eb73
Your Name <your.email@example.com>
2024-08-31 12:00:00 -0500

    August 31, 2024: Implemented feature X and fixed bug Y

--- src/main/CS_PgrowthHungryAliya/Main.java
+++ src/main/CS_PgrowthHungryAliya/Main.java
@@ -1,4 +1,17 @@
 package CS_PgrowthHungryAliya;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            printLine("one line");
+            System.out.println("i = " + i);
+            System.out.println("more");
+        }
+    }
+    public static void printLine(String line) {
+        System.out.println(line);
+    }
+
 }
---
+++ src/main/CS_PgrowthHungryAliya/classIntro/Person.java
@@ -0,0 +1,21 @@
+package CS_PgrowthHungryAliya.classIntro;
+
+public class Person {
+    private String name;
+    private int age;
+    // parameterized constructor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // copy constructor
+    public Person(Person anotherPerson) {
+        this.name = anotherPerson.name;
+        this.age = anotherPerson.age;
+    }
+
+
+
+
+}
