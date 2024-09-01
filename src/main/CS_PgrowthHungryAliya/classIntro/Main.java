2c9090ce90feb6d3b0910d3e425a6660e21596d6
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-09-01 18:09:33 -0500

    Computer Systems and Programming - GH Aliya

---
+++ src/main/CS_PgrowthHungryAliya/classIntro/Main.java
@@ -0,0 +1,11 @@
+package CS_PgrowthHungryAliya.classIntro;
+
+public class Main {
+    public static void main(String[] args) {
+        Person aliya = new Person("Aliya", 28);
+        Person copy = new Person(aliya);
+
+        System.out.println("My copy object name is " + copy.getName());
+        System.out.println("My copy object age is "+ copy.getAge());
+    }
+}
