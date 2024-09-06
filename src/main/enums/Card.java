b972cc836a982effe67383190388f8ebb5cbbb17
tamaratsymbaliuk <tsymbalt14@gmail.com>
2024-09-05 21:57:52 -0500

    Udemy - enum program

--- src/main/enums/Card.java
+++ src/main/enums/Card.java
@@ -32,9 +32,6 @@ public class Card {
     }
 
     public int getValue() {
-        return switch (this.rank) {
-            case JACK, QUEEN, KING -> 10;
-            default -> this.rank.ordinal() + 1;
-        };
+        return this.rank.getValue();
     }
 }
